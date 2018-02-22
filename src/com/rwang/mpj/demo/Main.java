9f78ec7f0f8829b1cf43b4622ac32deb900178e7
tank720 <wangrui720@gmail.com>
2018-02-22 17:05:57 -0500

    Test again

--- src/com/rwang/mpj/demo/Main.java
+++ src/com/rwang/mpj/demo/Main.java
@@ -1,17 +1,14 @@
 package com.rwang.mpj.demo;
 
-import mpi.*;
+import mpi.MPI;
 
 public class Main {
 
-    public static void main(String args[]){
-
+    public static void main(String[] args) {
         MPI.Init(args);
-
         int me = MPI.COMM_WORLD.Rank();
         int size = MPI.COMM_WORLD.Size();
-
-        System.out.println("Hi from <"+me+"> of " + size );
+        System.out.println("Hello world from <"+me+"> from <"+size);
         MPI.Finalize();
     }
 }
